1ad276ca575e3ecefa22b03198f77046a0ba9db3
KimDongHee <donghee0901@naver.com>
2020-11-05 23:00:24 +0900

    Cleanup code

--- app/src/main/java/com/example/memo_kotlin/MemoAdapter.kt
+++ app/src/main/java/com/example/memo_kotlin/MemoAdapter.kt
@@ -1,15 +1,16 @@
 package com.example.memo_kotlin
 import android.content.Intent
+import android.os.Build.ID
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.memobar.view.*
 
 
-class MemoAdapter internal constructor(list: List<MemoEntity>?) :
+class MemoAdapter internal constructor(val list: List<MemoEntity>?) :
     RecyclerView.Adapter<MemoAdapter.ViewHolder>() {
-    private var mData: List<MemoEntity>? = null
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -23,36 +24,28 @@ class MemoAdapter internal constructor(list: List<MemoEntity>?) :
         holder: ViewHolder,
         position: Int
     ) {
-        holder.title.text = mData!![position].title
-        holder.content.text = mData!![position].content
-        holder.ID.text = mData!![position].id.toString()
+        holder.itemView.title.text = list!![position].title
+        holder.itemView.content.text = list!![position].content
+        holder.itemView.ID.text = list!![position].id.toString()
     }
 
     override fun getItemCount(): Int {
-        return mData!!.size
+        return list!!.size
     }
 
     // 아이템 뷰를 저장하는 뷰홀더 클래스.
     inner class ViewHolder internal constructor(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
-        var title = itemView.findViewById<TextView>(R.id.title)
-        var content = itemView.findViewById<TextView>(R.id.content)
-        var ID = itemView.findViewById<TextView>(R.id.ID)
 
         init {
             itemView.setOnClickListener {
                 val i = Intent(itemView.context, MemoViewActivity::class.java)
-                i.putExtra("title", title.text.toString())
-                i.putExtra("content", content.text.toString())
-                i.putExtra("id", ID.text.toString())
+                i.putExtra("title", itemView.title.text.toString())
+                i.putExtra("content", itemView.content.text.toString())
+                i.putExtra("id", itemView.ID.text.toString())
                 itemView.context.startActivity(i) //새 액티비티 띄우기
             }
         }
-    }
-
-    // 생성자에서 데이터 리스트 객체를 전달받음.
-    init {
-        mData = list
     }
 }
 
